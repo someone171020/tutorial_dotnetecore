b0e3af7a9cd5c3a5a3e87848ad47961898fec94c
Mengci Wu <mengci.wu@avanade.com>
2017-10-29 15:23:57 +0800

    Refactor VR to V using LINQ

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -23,19 +23,24 @@ namespace hwapp.Mapping
                 .ForMember(v => v.contactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
                 .ForMember(v => v.contactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
                 .ForMember(v => v.Features, opt => opt.Ignore())
-                .AfterMap((vr, v) => {
-                    var removedFeatures = new System.Collections.Generic.List<VehicleFeature>();
+                .AfterMap((vr, v) => {                    
                     // remove unselected features
-                    foreach (var f in v.Features)
-                        if (!vr.Features.Contains(f.FeatureId))
-                            removedFeatures.Add(f);
+                    // var removedFeatures = new System.Collections.Generic.List<VehicleFeature>();
+                    // foreach (var f in v.Features)
+                    //     if (!vr.Features.Contains(f.FeatureId))
+                    //         removedFeatures.Add(f);
+                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId));
                     foreach (var f in removedFeatures)
                         v.Features.Remove(f);
 
                     // add new features
-                    foreach (var id in vr.Features)
-                        if (!v.Features.Any(f => f.FeatureId == id))
-                            v.Features.Add(new VehicleFeature {FeatureId = id});
+                    // foreach (var id in vr.Features)
+                    //     if (!v.Features.Any(f => f.FeatureId == id))
+                    //         v.Features.Add(new VehicleFeature {FeatureId = id});
+                    
+                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature{FeatureId = id});
+                    foreach (var f in addedFeatures)
+                        v.Features.Add(f);
                 });
         }
     }
